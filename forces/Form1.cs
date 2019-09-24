2850126daa342394467f92d661debbcb3bdbf2d2
Declan Hitchmough <rc0037188@student.riversidecollege.ac.uk>
2019-09-24 12:46:37 +0100

    Add project files.

---
+++ forces/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double sin(double X)
+        {
+            return (Math.Sin(X * Math.PI / 180.0));
+        }
+        double asin(double X)
+        {
+            return (Math.Asin(X) * 180 / Math.PI);
+        }
+    }
+}
